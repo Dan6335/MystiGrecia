7914bb7e2c5ae265cef9e3409fcee06e91ad8aa9
Mattias150784 <Mattiasayenew@gmail.com>
2024-01-19 14:51:21 -0800

    Added -Bronze Block -Bronze Ore -Deepslate Bronze Ore -Recipes -Smelting

--- src/main/java/net/mattias/mystigrecia/Mysti.java
+++ src/main/java/net/mattias/mystigrecia/Mysti.java
@@ -1,6 +1,7 @@
 package net.mattias.mystigrecia;
 
 import com.mojang.logging.LogUtils;
+import net.mattias.mystigrecia.block.ModBlocks;
 import net.mattias.mystigrecia.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,7 @@ public class Mysti {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
